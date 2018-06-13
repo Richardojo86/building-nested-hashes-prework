e13108270108f6279207ccc78381442118fbef2c
richardojo04@gmail.com <richardojo04@gmail.com>
2018-06-13 19:40:26 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -22,8 +22,7 @@ def fourth_challenge
           :age => "30",
           :attitude => "confused" 
         },
-        { 
-          :name => "Nurse",
+        { :name => "Nurse",
           :age => "44",
           :attitude => "worried"}
       ]
