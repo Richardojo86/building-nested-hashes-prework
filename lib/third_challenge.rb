c39999da1569767f3547142f25760d4b1a1d1192
richardojo04@gmail.com <richardojo04@gmail.com>
2018-06-13 18:42:29 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -20,7 +20,7 @@ def third_challenge
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet"
-        :age => ""
+        :age => "50"
         },
       :matriarch => {
         #your key/value pairs here
