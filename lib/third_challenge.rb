fcefd85bbc685563e3c2113730c3a7aab549b913
richardojo04@gmail.com <richardojo04@gmail.com>
2018-06-13 18:39:55 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,16 +3,16 @@ def third_challenge
    :montague => {
       :patriarch => {
         :name => "Lord Montague"
-        :age => 53
+        :age => "53"
         
         },
       :matriarch => {
         :name => "Lady Montague"
-        :age => 54
+        :age => "54"
         },
       :hero => {
         :name => "Romeo"
-        :age => 1
+        :age => "1"
         },
       :hero_friends => []
    }, 
